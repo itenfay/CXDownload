9a7b5cb7e6a5287e067cafa8807370d44494fc61
chenxing <chenxing640@foxmail.com>
2023-06-02 14:49:23 +0800

    update

--- Example/CXDownload/AppDelegate.swift
+++ Example/CXDownload/AppDelegate.swift
@@ -18,12 +18,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        #if DEBUG
-        CXDownloadManager.shared.configuration.enableLog = true
-        #else
-        CXDownloadManager.shared.configuration.enableLog = false
-        #endif
-        
         return true
     }
 
