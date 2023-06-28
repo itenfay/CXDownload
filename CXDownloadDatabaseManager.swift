89be33f32f988ecf4d6433f5f028c7002143039f
chenxing <chenxing640@foxmail.com>
2023-06-28 17:58:51 +0800

    ignore file and add new files

--- CXDownloadDatabaseManager.swift
+++ CXDownloadDatabaseManager.swift
@@ -6,7 +6,34 @@
 //
 
 import UIKit
+import FMDB
 
 class CXDownloadDatabaseManager: NSObject {
-
+    
+    @objc public static let shared = CXDownloadDatabaseManager()
+    
+    private var dbQueue: FMDatabaseQueue!
+    
+    private override init() {
+        super.init()
+        self.createTable()
+    }
+    
+    func createTable() {
+        let path = CXDFileUtils.cachePath(withPathComponent: "dl.db")?.cxd_appendingPathComponent("x.f").cxd_path
+        
+        // Create db queue using path.
+        dbQueue = FMDatabaseQueue(path: path)
+        
+        dbQueue.inDatabase { db in
+            let sql = "CREATE TABLE IF NOT EXISTS x();"
+            do {
+                try db.executeUpdate(sql, values: nil)
+                CXDLogger.log(message: "Creating table is OK.", level: .info)
+            } catch {
+                CXDLogger.log(message: "Creating table is failed.: \(error.localizedDescription)", level: .error)
+            }
+        }
+    }
+    
 }
--- CXDownloadModel.swift
+++ CXDownloadModel.swift
@@ -7,6 +7,14 @@
 
 import UIKit
 
-class CXDownloadModel: NSObject {
-
+@objcMembers public class CXDownloadModel: NSObject {
+    
+    public var identifer: String = ""
+    public var url: String = ""
+    public var hashCode: String = ""
+    public var filePath: String = ""
+    public var totalFileSize: Int64 = 0
+    public var tmpFileSize: Int64 = 0
+    public var progress: Float = 0
+    
 }
