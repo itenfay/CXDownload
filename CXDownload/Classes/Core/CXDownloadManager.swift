ed244cd83872362aad4cade5cdc75ea4ab473390
chenxing <chenxing640@foxmail.com>
2023-06-30 17:09:58 +0800

    update code logic

--- CXDownload/Classes/Core/CXDownloadManager.swift
+++ CXDownload/Classes/Core/CXDownloadManager.swift
@@ -24,98 +24,105 @@ import Foundation
     }
 }
 
-typealias ProgressClosure = (_ progress: Float) -> Void
-typealias SuccessClosure = (_ filePath: String) -> Void
-typealias FailureClosure = (_ code: Int, _ message: String) -> Void
-typealias FinishClosure = (_ url: String) -> Void
-typealias StateChangeClosure = (_ state: CXDownloadState) -> Void
+/// Schedules a block asynchronously for execution on main thread.
+internal func runOnMainThread(block: @escaping () -> Void) {
+    DispatchQueue.main.async(execute: block)
+}
 
-public class CXDownloadManager {
+/// Schedules a block asynchronously for execution after delay.
+internal func asyncExec(afterDelay delay: TimeInterval = 0.2, block: @escaping () -> Void) {
+    DispatchQueue.main.asyncAfter(deadline: .now() + delay, execute: block)
+}
+
+typealias CXDownloadCallback = (_ model: CXDownloadModel) -> Void
+
+public class CXDownloadManager: NSObject {
     
     /// Returns a singleton instance.
-    public static let shared = CXDownloadManager()
+    @objc public static let shared = CXDownloadManager()
     
     /// Privatizes the constructor.
-    private init() {}
-    
-    /// The dictionary stores the downloader.
-    private var downloaderDict: [String : CXDownloadProcessor] = [:]
-    
-    /// The configuration for the download.
-    public struct Configuration {
-        /// The max count for the download.
-        public var maxDownloadCount: Int
+    private override init() {
+        super.init()
+        self.setup()
     }
     
-    /// Initializes an configuration instance, default log is enabled.
-    public var configuration = Configuration(maxDownloadCount: 1)
+    /// The dictionary stores the download processor.
+    private var downloadTaskDict: [String : CXDownloadTaskProcessor] = [:]
+    /// The dictionary stores the download date.
+    private var downloadDateDict: [String : Date] = [:]
     
-    /// Executes an asynchronous download with the url and some callback closures.
-    @discardableResult
-    public func download(url: String, progress: @escaping ProgressClosure, success: @escaping SuccessClosure, failure: @escaping FailureClosure) -> CXDownloader? {
-        guard let urlSha2 = url.cxd_sha2, !urlSha2.isEmpty else {
-            CXDLogger.log(message: "The url md5 is empty.", level: .info)
-            failure(CXDownloader.DownloadError.error(code: -1999, message: "The url md5 is empty."))
-            return nil
-        }
-        //CXDLogger.log(message: "urlSha2: \(urlSha2)", level: .info)
-        if let resultDict = downloaderDict.first(where: { $0.key == urlSha2 }) {
-            let _downloader = resultDict.value
-            CXDLogger.log(message: "Downloader: \(_downloader)", level: .info)
-            return _downloader
-        }
-        // Creates a downloader instance.
-        let downloader = CXDownloader.download(url: url,
-                                               progess: progress,
-                                               success: success,
-                                               failure: failure) {
-            [unowned self] urlString in
-            if let key = urlString.cxd_sha2 {
-                CXDLogger.log(message: "Remove key: \(key)", level: .info)
-                self.downloaderDict.removeValue(forKey: key)
-            }
-        }
-        downloaderDict[urlSha2] = downloader
-        downloader.startDownloading()
-        return downloader
+    /// The count of downloads currently in progress.
+    public var currentCount: Int = 0
+    /// The max concurrent count for the download.
+    public var maxConcurrentCount: Int = 1
+    /// Whether to allow cellular network download.
+    public var allowsCellularAccess: Bool = false
+    
+    private var networkReachabilityStatus: String = ""
+    private var queue: OperationQueue!
+    private var session: URLSession!
+    
+    private func setup() {
+        currentCount = 0
+        let ud = UserDefaults.standard
+        let tmaxConcurrentCount = ud.integer(forKey: CXDownloadConfig.maxConcurrentCountKey)
+        maxConcurrentCount = tmaxConcurrentCount > 0 ? tmaxConcurrentCount : 1
+        allowsCellularAccess = ud.bool(forKey: CXDownloadConfig.allowsCellularAccessKey)
+        
+        // Single-threaded proxy queue.
+        queue = OperationQueue()
+        queue.maxConcurrentOperationCount = 1
+        
+        // Defines the background session identifier.
+        let configuration = URLSessionConfiguration.background(withIdentifier: "CXDownloadBackgroundSessionIdentifier")
+        // Allows cellular network download, the default is YES, which is turned on here. We added a variable to control the user's switching choice.
+        configuration.allowsCellularAccess = true
+        
+        // Create `URLSession`, configure information, proxy, proxy thread.
+        session = URLSession(configuration: configuration, delegate: self, delegateQueue: queue)
+        
+        let notiCenter = NotificationCenter.default
+        notiCenter.addObserver(self, selector: #selector(onDownloadMaxConcurrentCountChange(_:)), name: CXDownloadConfig.maxConcurrentCountChangeNotification, object: nil)
+        notiCenter.addObserver(self, selector: #selector(onDownloadAllowsCellularAccessChange(_:)), name: CXDownloadConfig.allowsCellularAccessChangeNotification, object: nil)
+        notiCenter.addObserver(self, selector: #selector(networkingReachabilityDidChange(_:)), name: CXDownloadConfig.networkingReachabilityDidChangeNotification, object: nil)
     }
     
     /// Executes an asynchronous download with the url and some callback closures.
-    @discardableResult
-    public func download(url: String, customDirectory: String?, customFileName: String?, progress: @escaping CXDownloader.ProgressClosure, success: @escaping CXDownloader.SuccessClosure, failure: @escaping CXDownloader.FailureClosure) -> CXDownloader? {
-        guard let urlSha2 = url.cxd_sha2, !urlSha2.isEmpty else {
-            CXDLogger.log(message: "The url md5 is empty.", level: .info)
-            failure(CXDownloader.DownloadError.error(code: -1999, message: "The url md5 is empty."))
-            return nil
+    public func download(url: String,
+                         progress: @escaping (CXDownloadModel) -> Void,
+                         success: @escaping (CXDownloadModel) -> Void,
+                         failure: @escaping (CXDownloadModel) -> Void)
+    {
+        // It is forbidden to call the same task repeatedly within 1.0s
+        if let date = downloadDateDict[url], Date().timeIntervalSince(date) < 1.0 {
+            return
         }
-        //CXDLogger.log(message: "urlSha2: \(urlSha2)", level: .info)
-        if let resultDict = downloaderDict.first(where: { $0.key == urlSha2 }) {
-            let _downloader = resultDict.value
-            CXDLogger.log(message: "Downloader: \(_downloader)", level: .info)
-            return _downloader
+        downloadDateDict[url] = Date()
+        var downloadModel = CXDownloadDatabaseManager.shared.getModel(by: url)
+        if downloadModel == nil {
+            downloadModel = CXDownloadModel()
+            downloadModel?.url = url
+            downloadModel?.fid = url.cxd_sha2
+            CXDownloadDatabaseManager.shared.insertModel(downloadModel!)
         }
-        // Creates a downloader instance.
-        let downloader = CXDownloader.download(url: url,
-                                               customDirectory: customDirectory,
-                                               customFileName: customFileName,
-                                               progess: progress,
-                                               success: success,
-                                               failure: failure) {
-            [unowned self] urlString in
-            if let key = urlString.cxd_sha2 {
-                CXDLogger.log(message: "Remove key: \(key)", level: .info)
-                self.downloaderDict.removeValue(forKey: key)
-            }
-        }
-        downloaderDict[urlSha2] = downloader
-        downloader.startDownloading()
-        return downloader
+    }
+    
+    /// Executes an asynchronous download with the url and some callback closures.
+    public func download(url: String,
+                         customDirectory: String?,
+                         customFileName: String?,
+                         progress: @escaping (CXDownloadModel) -> Void,
+                         success: @escaping (CXDownloadModel) -> Void,
+                         failure: @escaping (CXDownloadModel) -> Void)
+    {
+        
     }
     
     /// Resumes a download task through a specified url.
     public func resume(with url: String) {
-        _ = downloaderDict.first {
-            if $0.key == url.cxd_sha2 { $0.value.resume()
+        _ = downloadTaskDict.first {
+            if $0.key == url.cxd_sha2 { $0.value.resumeTask()
                 return true
             } else { return false }
         }
@@ -123,8 +130,8 @@ public class CXDownloadManager {
     
     /// Pauses a download task through a specified url.
     public func pause(with url: String) {
-        _ = downloaderDict.first {
-            if $0.key == url.cxd_sha2 { $0.value.pause()
+        _ = downloadTaskDict.first {
+            if $0.key == url.cxd_sha2 { $0.value.pauseTask()
                 return true
             } else { return false }
         }
@@ -132,8 +139,8 @@ public class CXDownloadManager {
     
     /// Cancels a download task through a specified url.
     public func cancel(with url: String) {
-        _ = downloaderDict.first {
-            if $0.key == url.cxd_sha2 { $0.value.cancel()
+        _ = downloadTaskDict.first {
+            if $0.key == url.cxd_sha2 { $0.value.cancelTask()
                 return true
             } else { return false }
         }
@@ -141,17 +148,17 @@ public class CXDownloadManager {
     
     /// Resumes the all download tasks.
     public func resumeAll() {
-        downloaderDict.forEach { $0.value.resume() }
+        downloadTaskDict.forEach { $0.value.resumeTask() }
     }
     
     /// Pauses the all download tasks.
     public func pauseAll() {
-        downloaderDict.forEach { $0.value.pause() }
+        downloadTaskDict.forEach { $0.value.pauseTask() }
     }
     
     /// Cancels the all download tasks.
     public func cancelAll() {
-        downloaderDict.forEach { $0.value.cancel() }
+        downloadTaskDict.forEach { $0.value.cancelTask() }
     }
     
     /// Removes the target file through a specified url, the target directory and the custom filename.
@@ -165,14 +172,59 @@ public class CXDownloadManager {
     
     /// Cleans up the invalid download tasks.
     public func cleanUpInvalidTasks() {
-        for key in downloaderDict.keys {
-            guard let downloader = downloaderDict[key] else {
+        for key in downloadTaskDict.keys {
+            guard let processor = downloadTaskDict[key] else {
                 continue
             }
-            if downloader.state == .success || downloader.state == .failed || downloader.state == .cancelled {
-                downloaderDict.removeValue(forKey: key)
+            if processor.state == .finish || processor.state == .error || processor.state == .cancelled {
+                downloadTaskDict.removeValue(forKey: key)
             }
         }
+    }
+    
+    deinit {
+        session.invalidateAndCancel()
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+}
+
+//MARK: - URLSessionDataDelegate
+
+extension CXDownloadManager: URLSessionDataDelegate {
+    
+    public func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse, completionHandler: @escaping (URLSession.ResponseDisposition) -> Void) {
+        
+    }
+    
+    public func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        
+    }
+    
+    public func urlSession(_ session: URLSession, didBecomeInvalidWithError error: Error?) {
+        
+    }
+    
+    public func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        
+    }
+    
+}
+
+//MARK: - Notification
+
+extension CXDownloadManager {
+    
+    @objc private func onDownloadMaxConcurrentCountChange(_ notification: Notification) {
+        
+    }
+    
+    @objc private func onDownloadAllowsCellularAccessChange(_ notification: Notification) {
+        
+    }
+    
+    @objc private func networkingReachabilityDidChange(_ notification: Notification) {
+        
     }
     
 }
