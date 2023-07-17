f34f24f0a4076d41e5d9307fb1fec76b4a6a7bed
chenxing <chenxing640@foxmail.com>
2023-07-17 14:17:25 +0800

    update

--- CXDownload/Classes/Extension/Button+CXDL.swift
+++ CXDownload/Classes/Extension/Button+CXDL.swift
@@ -20,9 +20,9 @@ extension CXDownloadBase where T : CXDButton {
         url: String,
         toDirectory directory: String? = nil,
         fileName: String? = nil,
-        progress: ((CXDownloadModel) -> Void)?,
-        success: ((CXDownloadModel) -> Void)?,
-        failure: ((CXDownloadModel) -> Void)?)
+        progress: @escaping (CXDownloadModel) -> Void,
+        success: @escaping (CXDownloadModel) -> Void,
+        failure: @escaping (CXDownloadModel) -> Void)
     {
         /*
         return CXDownloadManager.shared.download(url: url, toDirectory: directory, fileName: fileName, progress: { [weak _base = self.base] model in
